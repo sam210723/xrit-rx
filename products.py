05a3ce958a67c2f656c6f27a99eb6551b18b759d
Sam <sam210723@hotmail.com>
2020-02-11 17:17:38 +1000

    Product class

---
+++ products.py
@@ -0,0 +1,13 @@
+"""
+products.py
+https://github.com/sam210723/xrit-rx
+
+Parsing and assembly functions for downlinked products
+"""
+
+class Product:
+    """
+    Product base class
+    """
+
+    pass
