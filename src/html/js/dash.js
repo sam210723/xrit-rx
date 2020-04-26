9a164154af613facb149d69e17ad7d7c727ab794
Sam <sam210723@hotmail.com>
2020-04-26 22:08:22 +1000

    Configuration success check

--- src/html/js/dash.js
+++ src/html/js/dash.js
@@ -1,18 +1,45 @@
+/**
+ *  dash.js
+ *  https://github.com/sam210723/xrit-rx
+ *  
+ *  Updates dashboard data through xrit-rx REST API
+ */
+
+var config = {};
+
 function init()
 {
     print("Starting xrit-rx dashboard...", "DASH");
 
-    configure();
+    // Configure dashboard
+    ok = configure();
+    if (!ok) { return; }
+    
+    print("Ready", "DASH");
 }
 
 
 /**
- * Configure dashboard elements
+ * Configure dashboard
  */
 function configure()
 {
     print("Getting dashboard configuration...","CONF");
-    
-    config = JSON.parse(http_get("/api"))
+
+    // Get config object from xrit-rx
+    res = http_get("/api");
+    if (res) {
+        config = JSON.parse(res);
+    }
+    else {
+        print("Failed to get configuration", "CONF");
+        return false;
+    }
+
+    // Write config object to console
     console.log(config);
+
+    return true;
+}
+
 }
